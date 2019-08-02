b45395f6e20bb68294a0f93df25cc0144b4b4cef
Jardelmc <jardel.casteluber@gmail.com>
2019-08-02 00:58:13 -0300

    feature - Add styles to login form

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import logo from '../../assets/instagram.svg';
+
+const schema = Yup.object().shape({
+  login: Yup.string().required('Por favor, preencha este campo'),
+  password: Yup.string().required('A senha é obrigatória'),
+});
 
 export default function SignIn() {
-  return <h1>Porra</h1>;
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  return (
+    <>
+      <img src={logo} alt="Instagram" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="login" type="text" placeholder="Usuário no Instagram" />
+        <Input name="password" type="password" placeholder="Senha" />
+
+        <button type="submit">Acessar</button>
+      </Form>
+    </>
+  );
 }
